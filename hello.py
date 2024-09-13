6ed1b4695e76e5a57154f5b71443ee1f23a50285
nemasha-demel <nemashahirushidemel@gmail.com>
2024-09-13 22:45:42 +0530

    Variables

---
+++ hello.py
@@ -0,0 +1,9 @@
+name , age, weight = "Nemasha" , 22 , 43.5
+
+print("Your name is: " + name)
+
+#print("You are " + age + " years old") #can only concatenate str to str
+print("You are" , age , "years old")
+
+print(weight)
+
